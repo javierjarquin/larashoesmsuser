119030054c5ac71e952c868554616aa8829a4711
javier jarquin <javier.jarquin.chooc@gmail.com>
2023-11-25 16:33:15 -0500

    feat: se termina función para crear un usuario

--- confg/connection.py
+++ confg/connection.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base
 
-URL_DATABASE = 'mysql+pymysql://erp_shoes:lara1234*@localhost:3306/larashoesdb'
+URL_DATABASE = 'mysql+pymysql://root:lara1234*@localhost:3306/larashoesdb'
 
 engine = create_engine(URL_DATABASE)
 
--- msuser/Scripts/activate.bat
+++ msuser/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\javier\Documents\workbench\laraShoes\ms\user\msuser
+set VIRTUAL_ENV=C:\Users\Desarrollo Backend\Documents\workbech\proyect\larashoesmsuser\msuser
 
 if not defined PROMPT set PROMPT=$P$G
 
